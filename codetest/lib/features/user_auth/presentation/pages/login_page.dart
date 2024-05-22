3bac6f608f88e590d5334556d6c3dbe74f02e4f8
JOEL GARY <b032110294@student.utem.edu.my>
2024-05-23 00:17:09 +0800

    Created Login and Sign up Page successfully without authentication

--- codetest/lib/features/user_auth/presentation/pages/login_page.dart
+++ codetest/lib/features/user_auth/presentation/pages/login_page.dart
@@ -1,3 +1,7 @@
+import 'package:codetest/features/user_auth/presentation/pages/home_page.dart';
+import 'package:codetest/features/user_auth/presentation/pages/signUpPage.dart';
+import 'package:codetest/features/user_auth/presentation/widgets/form_container_widget.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
@@ -9,13 +13,59 @@ class LoginPage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Login"),
       ),
-      body: Column(
-        children: [
-          Text(
-            "Login",
-            style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold),
-          )
-        ],
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 300),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Login",
+              style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            FormContainerWidget(
+              hintText: "Email",
+              isPasswordField: false,
+            ),
+            SizedBox(height: 10),
+            FormContainerWidget(
+              hintText: "Password",
+              isPasswordField: true,
+            ),
+            SizedBox(height: 30,),
+            GestureDetector(
+              onTap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => HomePage()));
+              },
+              child: Container(
+                width: double.infinity,
+                height: 50,
+                decoration: BoxDecoration(
+                    color: Colors.red,
+                    borderRadius: BorderRadius.circular(10)
+                ),
+                child: Center(child: Text("Login", style: TextStyle(
+                    color: Colors.white, fontWeight: FontWeight.bold),)),
+              ),
+            ),
+            SizedBox(height: 20),
+            Row(mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("Don't have an account?"),
+              SizedBox(width: 5),
+              GestureDetector(
+                onTap: (){
+                  Navigator.pushAndRemoveUntil(context,
+                      MaterialPageRoute(builder: (context) => SignUpPage()),(route) => false);
+                },
+                child: Text("Sign Up", style: TextStyle(color: Colors.red,fontWeight: FontWeight.bold),),
+              )
+            ],)
+          ],
+        ),
       ),
     );
   }
---
+++ codetest/lib/features/user_auth/presentation/pages/signUpPage.dart
@@ -0,0 +1,73 @@
+import 'package:codetest/features/user_auth/presentation/pages/login_page.dart';
+import 'package:codetest/features/user_auth/presentation/widgets/form_container_widget.dart';
+import 'package:flutter/material.dart';
+
+class SignUpPage extends StatelessWidget {
+  const SignUpPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("SignUp"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 300),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Sign Up",
+              style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            FormContainerWidget(
+              hintText: "Username",
+              isPasswordField: false,
+            ),
+            SizedBox(height: 10),
+            FormContainerWidget(
+              hintText: "Email",
+              isPasswordField: false,
+            ),
+            SizedBox(height: 10),
+            FormContainerWidget(
+              hintText: "Password",
+              isPasswordField: true,
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Container(
+              width: double.infinity,
+              height: 50,
+              decoration: BoxDecoration(
+                  color: Colors.red, borderRadius: BorderRadius.circular(10)),
+              child: Center(
+                  child: Text(
+                "Sign Up",
+                style:
+                    TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+              )),
+            ),
+            SizedBox(height: 20),
+            Row(mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("Already have an account?"),
+                SizedBox(width: 5),
+                GestureDetector(
+                  onTap: (){
+                    Navigator.pushAndRemoveUntil(context,
+                        MaterialPageRoute(builder: (context) => LoginPage()),(route) => false);
+                  },
+                  child: Text("Login", style: TextStyle(color: Colors.red,fontWeight: FontWeight.bold),),
+                )
+              ],)
+          ],
+        ),
+      ),
+    );
+  }
+}
