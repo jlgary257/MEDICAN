738b23eff3d30cd034af6ecd30e7e698d2aea9e1
JOEL GARY <b032110294@student.utem.edu.my>
2024-06-17 04:39:10 +0800

    Done with Doctor domain may need editting in future
    close branch

--- codetest/lib/Doctor/doctor_info.dart
+++ codetest/lib/Doctor/doctor_info.dart
@@ -1,20 +1,109 @@
-
-
-
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:codetest/features/user_auth/presentation/widgets/form_container_widget.dart';
+import 'package:codetest/global/toast.dart';
 import 'package:flutter/material.dart';
 
 class UpdateDoctorInfo extends StatefulWidget {
-  const UpdateDoctorInfo({super.key});
+  final String Id;
+  const UpdateDoctorInfo({super.key, required this.Id});
 
   @override
   State<UpdateDoctorInfo> createState() => _UpdateDoctorInfoState();
 }
 
 class _UpdateDoctorInfoState extends State<UpdateDoctorInfo> {
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  String staffId = "2";
+
+  @override
+  void initState() {
+    super.initState();
+    _loadData(); // Load data when the widget is initialized
+  }
+
+  Future<void> _loadData() async {
+    final docSnapshot = await FirebaseFirestore.instance
+        .collection('Staff')
+        .doc(staffId)
+        .collection("Doctor")
+        .doc(widget.Id)
+        .get();
+
+    if (docSnapshot.exists) {
+      final data = docSnapshot.data()!;
+      setState(() {
+        _nameController.text = data['name'] ?? '';
+        _emailController.text = data['email'] ?? '';
+      });
+    }
+  }
+
+  void _updateDoctorData() async {
+    final doctorCollection = FirebaseFirestore.instance
+        .collection('Staff')
+        .doc(staffId)
+        .collection("Doctor");
+
+    final newDoctor = {
+      'name': _nameController.text,
+      'email': _emailController.text,
+    };
+
+    try {
+      await doctorCollection.doc(widget.Id).update(newDoctor);
+      showToast(message: "Doctor information updated successfully");
+    } catch (e) {
+      showToast(message: "Failed to update doctor information: $e");
+    }
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: MainAppBar(title: "Home Doctor"),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 200),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Text(
+              "Update Doctor Information",
+              style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 50),
+            FormContainerWidget(
+              controller: _emailController,
+              hintText: "Email",
+            ),
+            SizedBox(height: 10),
+            FormContainerWidget(
+              controller: _nameController,
+              hintText: "Name",
+            ),
+            SizedBox(height: 30),
+            GestureDetector(
+              onTap: _updateDoctorData,
+              child: Container(
+                width: double.infinity,
+                height: 45,
+                decoration: BoxDecoration(
+                    color: Colors.red, borderRadius: BorderRadius.circular(10)),
+                child: Center(
+                  child: Text(
+                    "Update",
+                    style: TextStyle(
+                        color: Colors.white, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+          ],
+        ),
+      ),
+    );
   }
 }
 
@@ -25,7 +114,6 @@ class DoctorModel {
   final String? StaffId;
 
   DoctorModel({this.name, this.email, this.DoctorId, this.StaffId});
-
 
   static DoctorModel fromSnapshot(
       DocumentSnapshot<Map<String, dynamic>> snapshot) {
@@ -42,7 +130,7 @@ class DoctorModel {
       "name": name,
       "email": email,
       "DoctorId": DoctorId,
-      "StaffId" : StaffId,
+      "StaffId": StaffId,
     };
   }
 }
--- codetest/lib/Doctor/home_dr.dart
+++ codetest/lib/Doctor/home_dr.dart
@@ -1,17 +1,10 @@
-
-
-
-
-
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:codetest/Doctor/doctor_info.dart';
 import 'package:codetest/Doctor/form_consult.dart';
 import 'package:codetest/Doctor/patient_info.dart';
 import 'package:codetest/features/user_auth/firebase_auth_implementation/password_change.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
-import '../features/user_auth/presentation/pages/login_page.dart';
-import '../features/user_auth/presentation/pages/signUpPage.dart';
 import '../features/user_auth/presentation/widgets/form_container_widget.dart';
 import '../global/toast.dart';
 
@@ -23,45 +16,101 @@ class homeDoctor extends StatefulWidget {
 }
 
 class _homeDoctorState extends State<homeDoctor> {
+  String? _doctorId;
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchDoctorId();
+  }
+
+  Future<void> _fetchDoctorId() async {
+    String email = FirebaseAuth.instance.currentUser?.email ?? '';
+
+    if (email.isNotEmpty) {
+      String? doctorId = await getDoctorIdByEmail(email);
+      setState(() {
+        _doctorId = doctorId;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: MainAppBar(title: "Home Doctor",),
+      appBar: MainAppBar(title: "Home Doctor"),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text("Welcome Dr.",style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold)),
-              SizedBox(height: 50,),
-              RedElevatedButton(onPressed:(){
+              Text("Welcome Dr.", style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold)),
+              SizedBox(height: 50),
+              RedElevatedButton(
+                onPressed: () {
                   Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => DrForm()));
-                  }   , text: "Diagnose Patient"),
-              SizedBox(height: 10,),
-              RedElevatedButton(onPressed:(){
+                    context,
+                    MaterialPageRoute(builder: (context) => DrForm()),
+                  );
+                },
+                text: "Diagnose Patient",
+              ),
+              SizedBox(height: 10),
+              RedElevatedButton(
+                onPressed: () {
                   Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => viewPatientInfo()));
-                  }   , text: "View Patient Information"),
-              SizedBox(height: 10,),
-              RedElevatedButton(onPressed:(){
+                    context,
+                    MaterialPageRoute(builder: (context) => viewPatientInfo()),
+                  );
+                },
+                text: "View Patient Information",
+              ),
+              SizedBox(height: 10),
+              RedElevatedButton(
+                onPressed: () {
+                  if (_doctorId != null) {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => UpdateDoctorInfo(Id: _doctorId!)),
+                    );
+                  } else {
+                    showToast(message: "Doctor ID not found");
+                  }
+                },
+                text: "Update Information",
+              ),
+              SizedBox(height: 10),
+              RedElevatedButton(
+                onPressed: () {
                   Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => UpdatePatientInfo(documentId: "06kifAuy9WP7ZZchOG0y")));
-                  }   , text: "Update Information"),
-              SizedBox(height: 10,),
-              RedElevatedButton(onPressed:(){
-                  Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => ChangePasswordScreen()));
-                  }   , text: "Change Password")
+                    context,
+                    MaterialPageRoute(builder: (context) => ChangePasswordScreen()),
+                  );
+                },
+                text: "Change Password",
+              ),
             ],
           ),
         ),
       ),
     );
+  }
+
+  Future<String?> getDoctorIdByEmail(String email) async {
+    print(email);
+    try {
+      final doctorCollection = FirebaseFirestore.instance.collection("Staff").doc("2").collection("Doctor");
+      final doctorQuerySnapshot = await doctorCollection.where('email', isEqualTo: email).get();
+
+      if (doctorQuerySnapshot.docs.isNotEmpty) {
+        return doctorQuerySnapshot.docs[0].id; // Return the document ID as the Doctor ID
+      }
+
+      return null; // Doctor ID not found
+    } catch (error) {
+      print('Error getting doctor ID: $error');
+      return null; // Handle potential errors
+    }
   }
 }
