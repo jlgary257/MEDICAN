bb28cf377cdb6ef12ed8d509d8e70c5d8d11afd7
JOEL GARY <b032110294@student.utem.edu.my>
2024-08-22 18:43:12 +0800

    Created System DV for Main system analytics

--- codetest/lib/data_visual/MainBI.dart
+++ codetest/lib/data_visual/MainBI.dart
@@ -1,4 +1,5 @@
 
+import 'package:codetest/data_visual/SystemDV.dart';
 import 'package:codetest/data_visual/yearCases.dart';
 import 'package:codetest/features/user_auth/presentation/widgets/form_container_widget.dart';
 import'package:flutter/material.dart';
@@ -30,7 +31,7 @@ class _MainBiState extends State<MainBi> {
                       onPressed: () {
                         Navigator.push(
                           context,
-                          MaterialPageRoute(builder: (context) => TraumaCase()),
+                          MaterialPageRoute(builder: (context) => SystemDataV()),
                         );
                       },
                       text: "Main Analytics",
---
+++ codetest/lib/data_visual/SystemDV.dart
@@ -0,0 +1,19 @@
+
+import 'package:codetest/features/user_auth/presentation/widgets/form_container_widget.dart';
+import 'package:flutter/material.dart';
+
+class SystemDataV extends StatelessWidget {
+  const SystemDataV({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: MainAppBar(title: 'Home Admin'),
+      body: GridView.count(
+        crossAxisCount: 2,children: [
+          Container(color: Colors.redAccent,)
+      ],
+      )
+    );
+  }
+}
